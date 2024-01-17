d44d1a018bdf13e71be94bf4d7a8d54b17d10ff9
Arga Roh Sahrijal Saragih <arga.saragih@gmail.com>
2024-01-17 20:53:49 +0700

    LC-1207 Py

---
+++ leetcode/1207-unique-number-of-occurrences/solve.py
@@ -0,0 +1,9 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        dict = {}
+        for num in arr:
+            if num in dict:
+                dict[num] += 1
+            else:
+                dict[num] = 1
+        return len(set(dict.values())) == len(dict.values())
